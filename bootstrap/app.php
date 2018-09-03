2caed78b1a869ddc3812c38fb86e843217e239e6
wanganlin <wanganlin@ecmoban.com>
2018-09-03 10:14:15 +0800

    更新入口脚本

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,25 +7,52 @@
  * @homepage https://ectouch.cn
  */
 
-// 检测PHP环境
+/*
+|--------------------------------------------------------------------------
+| 检测PHP环境
+|--------------------------------------------------------------------------
+|
+*/
+
 if (version_compare(PHP_VERSION, '7.1.3', '<')) {
     die('require PHP > 7.1.3 !');
 }
+
+/*
+|--------------------------------------------------------------------------
+| Setting Version
+|--------------------------------------------------------------------------
+|
+*/
 
 define('APPNAME', 'ECTouch');
 define('VERSION', 'v3.0.0');
 define('RELEASE', '20180808');
 define('EC_CHARSET', 'utf-8');
+
 define('ADMIN_PATH', 'admin');
 define('AUTH_KEY', 'this is a key');
 define('OLD_AUTH_KEY', '');
 define('API_TIME', '');
-define('APP_DEBUG', true);
-define('BUILD_DIR_SECURE', false);
-define('ROOT_PATH', str_replace('\\', '/', dirname(__DIR__)) . '/');
-define('APP_PATH', ROOT_PATH . 'app/');
-define('CONF_PATH', ROOT_PATH . 'config/');
-define('LANG_PATH', ROOT_PATH . 'resources/lang/');
-define('RUNTIME_PATH', ROOT_PATH . 'storage/framework/');
-require CONF_PATH . 'constant.php';
-require APP_PATH . 'Kernel/Loader.php';
+
+/*
+|--------------------------------------------------------------------------
+| Setting Debuger
+|--------------------------------------------------------------------------
+|
+*/
+
+if (!in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])) {
+    defined('APP_DEBUG') or define('APP_DEBUG', false);
+} else {
+    defined('APP_DEBUG') or define('APP_DEBUG', true);
+}
+
+/*
+|--------------------------------------------------------------------------
+| Loading Kernel
+|--------------------------------------------------------------------------
+|
+*/
+
+require dirname(__DIR__) . '/app/Kernel/Loader.php';
