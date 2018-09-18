8fccd130dc60db03b8820b645417b46178951170
wanganlin <wanganlin@ecmoban.com>
2018-09-18 14:08:22 +0800

    add react native

--- resources/client/app/App.js
+++ resources/client/app/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'antd-mobile-rn';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>dscmall app!</Text>
+        <Button>antd-mobile-rn button</Button>
       </View>
     );
   }
